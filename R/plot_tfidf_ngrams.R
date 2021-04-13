c2d4d0d355a14b54ac902919e6100fcf7720a1bb
Andreas <andreassot10@gmail.com>
2021-04-13 09:14:34 +0100

    Functions for module mod_tfidf.

---
+++ R/plot_tfidf_ngrams.R
@@ -0,0 +1,25 @@
+#' Title
+#'
+#' @param tfidf_ngrams
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+plot_tfidf_ngrams <- function(tfidf_ngrams, ngrams_type, filter_class) {
+
+  tfidf_ngrams %>%
+    ggplot2::ggplot(ggplot2::aes(tf_idf, reorder(ngram, tf_idf))) +
+    ggplot2::geom_col(fill = 'blue', alpha = 0.6) +
+    ggplot2::labs(x = "TF-IDF*", y = NULL,
+                  title = paste0("Most frequent ", ngrams_type,
+                                 " in feedback text that is about\n",
+                                 "\"", filter_class, "\"")) +
+    ggplot2::theme_bw() +
+    ggplot2::theme(
+      panel.grid.major = ggplot2::element_blank(),
+      panel.grid.minor = ggplot2::element_blank(),
+      axis.text.y = ggplot2::element_text(size = 12)
+    )
+}
