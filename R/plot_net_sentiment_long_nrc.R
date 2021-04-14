37b89231d5439ce769c0d58f0f0c77f9d8107b7c
Andreas <andreassot10@gmail.com>
2021-04-14 09:19:43 +0100

    code: Assign plot to object and return it.

--- R/plot_net_sentiment_long_nrc.R
+++ R/plot_net_sentiment_long_nrc.R
@@ -9,7 +9,7 @@
 
 plot_net_sentiment_long_nrc <- function(net_sentiment_long_nrc) {
 
-  net_sentiment_long_nrc %>%
+  p <- net_sentiment_long_nrc %>%
     ggplot2::ggplot(ggplot2::aes(value, name)) +
     ggplot2::geom_col(fill = "blue", alpha = 0.6) +
     ggplot2::facet_wrap(~ linenumber, ncol = 5) +
@@ -22,4 +22,6 @@ plot_net_sentiment_long_nrc <- function(net_sentiment_long_nrc) {
       axis.ticks.x = ggplot2::element_blank()
     ) +
     ggplot2::ylab('')
+
+  return(p)
 }
--- R/plot_tfidf_ngrams.R
+++ R/plot_tfidf_ngrams.R
@@ -9,7 +9,7 @@
 
 plot_tfidf_ngrams <- function(tfidf_ngrams, ngrams_type, filter_class) {
 
-  tfidf_ngrams %>%
+  p <- tfidf_ngrams %>%
     ggplot2::ggplot(ggplot2::aes(tf_idf, reorder(ngram, tf_idf))) +
     ggplot2::geom_col(fill = 'blue', alpha = 0.6) +
     ggplot2::labs(x = "TF-IDF*", y = NULL,
@@ -22,4 +22,6 @@ plot_tfidf_ngrams <- function(tfidf_ngrams, ngrams_type, filter_class) {
       panel.grid.minor = ggplot2::element_blank(),
       axis.text.y = ggplot2::element_text(size = 12)
     )
+
+  return(p)
 }
