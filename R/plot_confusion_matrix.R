b95bc395e06b343908d9342d5b85e8a24b5936e6
andreassot10 <andreassot10@gmail.com>
2021-05-28 13:02:30 +0100

    code: Make colour gradients in conf. mat. relative to columns.

--- R/plot_confusion_matrix.R
+++ R/plot_confusion_matrix.R
@@ -18,16 +18,46 @@ plot_confusion_matrix <- function(x, target_col_name, target_pred_col_name,
                                                   target_pred_col_name,
                                                   grouping_variables)
 
+  # Could use ggplot2::autoplot(cm). However, the gradient fills the heatmap
+  # (confusion matrix) based on absolute counts. It does not make sense to
+  # compare colours between different "Truth" columns. Below is a custom
+  # alternative that expresses column counts as proportions of the total count
+  # in that column. The proportions are used to define a colour scale. The
+  # actual counts are plotted though.
   cm %>%
     lapply(
       function(x) {
-        x %>%
-          ggplot2::autoplot(type) +
-          ggplot2::theme(
-            axis.text.x = ggplot2::element_text(angle = 90, vjust = 0.5,
-                                                hjust = 1),
-            axis.text = ggplot2::element_text(size = 15)
+
+        data_heatmap <- x$table %>%
+          as.data.frame() %>%
+          dplyr::group_by(Truth) %>%
+          dplyr::mutate(
+            prop = round(Freq / sum(Freq) * 100),
+            bin = dplyr::case_when(
+              dplyr::between(prop, 0, 9) ~ "gray88",
+              dplyr::between(prop, 10, 19) ~ "gray87",
+              dplyr::between(prop, 20, 29) ~ "gray73",
+              dplyr::between(prop, 30, 39) ~ "gray61",
+              dplyr::between(prop, 40, 59) ~ "gray57",
+              dplyr::between(prop, 60, 69) ~ "gray54",
+              dplyr::between(prop, 70, 79) ~ "gray48",
+              dplyr::between(prop, 80, 100) ~ "gray37",
+            )
           )
+
+        data_heatmap %>%
+          ggplot2::ggplot(ggplot2::aes(Truth, forcats::fct_rev(Prediction))) +
+          ggplot2::geom_tile(ggplot2::aes(fill = bin, colour = "")) + # Colour will create a grid. Whichever value will trigger it to draw the grid.
+          ggplot2::scale_fill_manual(values = sort(unique(data_heatmap$bin))) +
+          ggplot2::theme_minimal() +
+          ggplot2::theme(
+            legend.position = "none",
+            panel.grid = ggplot2::element_blank(),
+            axis.text.x = ggplot2::element_text(angle = 90, vjust = 0.5, hjust = 1),
+            axis.text = ggplot2::element_text(size = 15)
+          ) +
+          ggplot2::geom_text(ggplot2::aes(label = Freq)) +
+          ggplot2::ylab("Prediction")
       }
     )
 }
