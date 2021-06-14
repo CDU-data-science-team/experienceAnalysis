148ef5f41b34ff0b07e1a0668597df7fb4b56a0f
Andreas <andreassot10@gmail.com>
2021-06-14 17:23:32 +0100

    code: Predict unlabelled text with Scikit-learn pipeline.

---
+++ R/calc_predict_unlabelled_text.R
@@ -0,0 +1,71 @@
+#' Predict unlabelled text using a fitted Scikit-learn pipeline
+#'
+#' For internal use only!
+#'
+#' @param x A data frame with the text data to predict classes for. If
+#'     `file_path` (see below) is not `NULL`, then `x` should be `NULL`.
+#' @param sys_setenv A string in the form "path_to_python/python.exe",
+#'     indicating which Python to use (e.g. from a virtual environment).
+#' @param which_python Same as `sys_setenv`.
+#' @param which_venv A string that can be "conda", "miniconda" or "python".
+#' @param venv_name String. The name of the virtual environment.
+#' @param file_path A string in the form "path_to_data/filename.csv", where
+#'     "filename" is the name of the CSV file with the data. If `NULL`, the
+#'     function uses x.
+#' @param predictor A string with the name of the text column.
+#' @param pipe_path A string in the form "path_to_fitted_pipeline/pipeline.sav,"
+#'     where "pipeline" is the name of the SAV file with the fitted
+#'     `Scikit-learn` pipeline.
+#' @param preds_column A string with the user-specified name of the column that
+#'     will have the predictions. If `NULL` (default), then the name will be
+#'     `paste0(predictor, "_preds")`.
+#' @param column_names A vector of strings with the names of the columns of the
+#'     supplied data frame (incl. `predictor`) to be added to the returned data
+#'     frame. If `NULL`, then the only column in the returned data frame will be
+#'     `preds_column.`
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+calc_predict_unlabelled_text <- function(x, sys_setenv, which_python, which_venv,
+                                         venv_name,
+                                         file_path = NULL,
+                                         predictor, pipe_path,
+                                         preds_column = NULL,
+                                         column_names = NULL) {
+
+  Sys.setenv(RETICULATE_PYTHON = sys_setenv)
+  reticulate::use_python(which_python)
+
+  if (which_venv == 'conda') {
+    reticulate::use_condaenv(venv_name, required = TRUE)
+  } else if (which_venv == 'miniconda') {
+    reticulate::use_miniconda(venv_name, required = TRUE)
+  } else if (which_venv == 'python') {
+    reticulate::use_virtualenv(venv_name, required = TRUE)
+  }
+
+  factory_predict_unlabelled_text_r <-
+    reticulate::py_run_string(
+      "from pxtextmining.factories.factory_predict_unlabelled_text import factory_predict_unlabelled_text"
+    )$
+    factory_predict_unlabelled_text
+
+  if (is.null(file_path)) {
+    x_py <- reticulate::r_to_py(x)
+
+    predictions <- factory_predict_unlabelled_text_r(
+      file_path=file_path, dataset=x_py, predictor=predictor,
+      pipe_path=pipe_path, preds_column=preds_column,
+      column_names=reticulate::r_to_py(column_names))
+  } else {
+    predictions <- factory_predict_unlabelled_text_r(
+      file_path=file_path, dataset=NULL, predictor=predictor,
+      pipe_path=pipe_path, preds_column=preds_column,
+      column_names=reticulate::r_to_py(column_names))
+  }
+
+  return(predictions)
+}
