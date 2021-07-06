c1fbf048c82ed77724ba780cce0e0eb34afceb72
Andreas <andreassot10@gmail.com>
2021-07-06 11:25:01 +0100

    code: Function prep_python().

---
+++ R/prep_python.R
@@ -0,0 +1,27 @@
+#' Title
+#'
+#' @param sys_setenv
+#' @param which_python
+#' @param which_venv
+#' @param venv_name
+#' @param text_col_name
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
+prep_python <- function(sys_setenv, which_python, which_venv, venv_name,
+                        text_col_name) {
+
+  Sys.setenv(RETICULATE_PYTHON = sys_setenv)
+  reticulate::use_python(which_python)
+
+  if (which_venv == 'conda') {
+    reticulate::use_condaenv(venv_name, required = TRUE)
+  } else if (which_venv == 'miniconda') {
+    reticulate::use_miniconda(venv_name, required = TRUE)
+  } else if (which_venv == 'python') {
+    reticulate::use_virtualenv(venv_name, required = TRUE)
+  }
+}
